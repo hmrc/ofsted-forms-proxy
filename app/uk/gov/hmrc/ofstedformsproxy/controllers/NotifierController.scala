73706185f480f4adb9db5a67d4341a744f3ccf5e
Pasquale Gatto <pasquale.gatto@hmrc.digital.gov.uk>
2019-07-10 09:54:13 +0100

    MOR-66: Add logging

--- app/uk/gov/hmrc/ofstedformsproxy/controllers/NotifierController.scala
+++ app/uk/gov/hmrc/ofstedformsproxy/controllers/NotifierController.scala
@@ -20,6 +20,7 @@ import javax.inject.Inject
 import play.api.mvc.Action
 import uk.gov.hmrc.ofstedformsproxy.config.AppConfig
 import uk.gov.hmrc.ofstedformsproxy.handlers.{NotifierRequestHandler, NotifyRequest, _}
+import uk.gov.hmrc.ofstedformsproxy.logging.OfstedFormProxyLogger
 import uk.gov.hmrc.ofstedformsproxy.notification.{Notifier, OfstedNotificationClient}
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
 
@@ -27,10 +28,11 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Success, Try}
 
 //TODO remove Guice!
-class NotifierController @Inject()(config: AppConfig)(implicit ex: ExecutionContext) extends BaseController {
+class NotifierController @Inject()(config: AppConfig, logger: OfstedFormProxyLogger)(implicit ex: ExecutionContext) extends BaseController {
 
   def sendNotification(): Action[NotifyRequest] = Action.async(parse.json[NotifyRequest]) {
     implicit request =>
+      logger.info(s"Notification request received $request")
       new NotifierRequestHandler[Try](new OfstedNotificationClient[Try](new Notifier[Try] {})).handleRequest(request.body) match {
         case Success(value) if value.status == 200 => Future.successful(Ok(""))
         case Success(value) => Future.successful(BadRequest(value.msg))
